1d485ad8ec20c84406dc0a93e5395352250b3ff4
ufotronic <tschueler737@gmail.com>
2025-08-10 16:35:42 +0200

    Define Models

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,12 +5,19 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
+    public function features(): HasMany{
+
+        return $this->hasMany(Feature::class);
+    }
+
+
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasRoles;
 
