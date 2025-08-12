088c6d666368edeaa702fd69ae86fd58a402ef64
ufotronic <tschueler737@gmail.com>
2025-08-12 12:24:55 +0200

    Create Feature Page and Display

--- resources/js/Components/FeatureItem.tsx
+++ resources/js/Components/FeatureItem.tsx
@@ -1,16 +1,52 @@
 import {Feature} from "@/types";
+import {useState} from "react";
 
 
 export default function FeatureItem({feature} : {feature: Feature}) {
 
+
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toogleReadMore = () => {
+
+    setIsExpanded(!isExpanded);
+
+  }
+
   return (
 
     <div className="mb-4 overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
-      <div className="p-6 text-gray-900 dark:text-gray-100">
+      <div className="p-6 text-gray-900 dark:text-gray-100 flex gap-8">
 
-      <pre>
-        {JSON.stringify(feature, undefined, 2)}
-      </pre>
+        <div className="flex flex-col items-center">
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                 stroke="currentColor" className="size-12">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5"/>
+            </svg>
+          </button>
+          <span className="text-2xl font-semibold">
+            12
+          </span>
+
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                 stroke="currentColor" className="size-12">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5"/>
+            </svg>
+          </button>
+
+
+        </div>
+
+        <div className="flex-1">
+          <h2 className="text-2xl mb-2">{feature.name}</h2>
+          <p>{isExpanded ? feature.description : `${feature.description.slice(0,200)}...`}</p>
+          <button onClick={toogleReadMore} className="text-amber-500 hover:underline">
+            {isExpanded ? 'Read Less' : 'Read More'}
+
+          </button>
+        </div>
 
       </div>
     </div>
